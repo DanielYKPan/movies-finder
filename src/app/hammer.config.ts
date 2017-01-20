e280a8cd186c0c0b1fa6cbe31872ced550e0bc3f
Yaokun Pan <myron.yk.pan@gmail.com>
2017-01-20 17:12:04 +1100

    Added hammer.config

--- src/app/hammer.config.ts
+++ src/app/hammer.config.ts
@@ -6,6 +6,7 @@ import { HammerGestureConfig } from "@angular/platform-browser";
 
 
 export class MyHammerConfig extends HammerGestureConfig  {
+    // https://github.com/hammerjs/hammer.js/issues/1014
     buildHammer(element: HTMLElement) {
         let mc = new Hammer(element, {
             touchAction: "pan-y",
